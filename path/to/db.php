5611df673cc7264c441cc9fdeaa12716f6855f20
rice762 <74892784+rice762@users.noreply.github.com>
2021-12-07 12:30:48 +0900

    Add files via upload

---
+++ path/to/db.php
@@ -0,0 +1,33 @@
+
+<?PHP
+    header('Content-Type: text/html; charset=UTF-8');
+    $db = new pdo("mysql:host=172.24.73.76/localhost;dbname=db_test", "root", "password");
+    
+    $sql = "SELECT * FROM order_table2";
+    // $sql = "SELECT * FROM table5 ORDER BY RAND() LIMIT 1";
+    $ps = $db->query($sql);
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <meta charset='utf-8'>
+        <title>練習</title>
+        <link rel="stylesheet" href="sample.css">
+        <script type="text/javascript" src="practice.js"></script>
+        <link rel="manifest" href="manifest.json">
+    </head>
+    <body>
+        <div>
+            <h1>練習</h1>
+            <p>コインを入れて下さい<br>飲み物は200円です</p>
+            <button onclick="buy()">飲み物を買う</button>
+            <button onclick="insert()">お金を入れる</button>
+            <?PHP
+                foreach ($ps as $val) {
+                    print '<table><tr><th>注文番号</th><th>テーブル番号</th><th>商品名</th><th>個数</th><tr>' .$val['order_num'] . '</tr>' .$val['table_num'] .$val['order_name'] .$val['quantity'] . '</tr></table>';
+                }
+            ?>
+        </div>
+    </body>
+</html>
